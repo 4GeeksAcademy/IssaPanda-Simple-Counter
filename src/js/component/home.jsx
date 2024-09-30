d2080a9403b5ff9eec66fa1b7197dc412441840d
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-09-30 17:42:08 +0000

    First commit(sorry for the wait)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+		const [counter, setCounter] = useState(0);
+
+		useEffect(() => {
+			const interval = setInterval(() => {
+			  setCounter(prevCounter => prevCounter + 1);
+			}, 1000);
+			return () => clearInterval(interval);
+		  }, []);
+
+		  const counterStr = counter.toString().padStart(6, '0');
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter-container">
+		  {counterStr.split('').map((digit, index) => (
+			<div key={index} className="digit">
+			  {digit}
+			</div>
+		  ))}
 		</div>
-	);
+	  );
 };
 
 export default Home;
